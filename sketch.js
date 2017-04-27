a480573a4de43420ebc1e9a2f59ef057c9b9b77b
JanicePark <JanicePark@janices-mbp.risd.edu>
2017-04-27 03:20:14 -0400

    index

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var x = 10;
 
 
 function setup() {
-    var theCanvas = createCanvas(1090, 1170);
+    var theCanvas = createCanvas(windowWidth, 1170);
     frameRate(30);
     stroke(0);
 }
@@ -14,7 +14,6 @@ function draw() {
     if (x < 0) {
         x = height;
     }
-
 
     line(x, 0, x, height);
     stroke((255), (0), (0));
